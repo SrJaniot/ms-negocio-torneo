44d7adf81bb0307c2a8a3cc76c7d281859212561
SrJaniot <estebanjaniot10@gmail.com>
2024-02-04 02:09:03 -0500

    CONFIGURACION DE VARIABLES DE ENTORNO

--- src/config/configuracion.seguridad.ts
+++ src/config/configuracion.seguridad.ts
@@ -23,4 +23,13 @@ export namespace ConfiguracionSeguridad{
   export const hostSeguridad="http://127.0.0.1:3000";
 
 
+
+  //-------------------------VARIABLES DE ENTORNO  -------------------------------------
+    //instalar el paquete dotenv npm i dotenv para poder leer variables de entorno  y importar en application.ts require('dotenv').config();
+    export const connection_user_postgres = process.env.CONNECTION_USER_POSTGRES ;
+    export const connection_password_postgres = process.env.CONNECTION_PASSWORD_POSTGRES ;
+    export const connection_database_postgres = process.env.CONNECTION_DATABASE_POSTGRES ;
+
+
+
 }
